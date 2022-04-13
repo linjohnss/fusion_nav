08204aa395371189f4489d8a4d4ac32c9491bd8f
linjohnss <linjohn0903@gmail.com>
2022-04-14 00:09:09 +0800

    Add navigation goal

--- src/navigation_goals.py
+++ src/navigation_goals.py
@@ -1,79 +1,46 @@
 #!/usr/bin/env python3
-# license removed for brevity
 
-from asyncio import sleep
-from tracemalloc import start
 import rospy
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from std_msgs.msg import String
 
-import time
+def main():
 
-pub = rospy.Publisher('amm', String, queue_size=10)
+    rospy.init_node('send_goal_python')
 
-def start_nav(x, y, w):
-    global pub
-    # print('start nav')
-    rospy.loginfo('start nav')
-    # pub.publish('Y')
-    try:
-        # rospy.init_node('movebase_client_py')
-        result = movebase_client(x, y, w)           
-        if result:
-            rospy.loginfo("Goal execution done!")
-            pub.publish('Y')
-            
-    except rospy.ROSInterruptException:
-        rospy.loginfo("Navigation test finished.")
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 
-    return
-
-def movebase_client(x, y, w):
-    print('---- move_base ----')
-    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+    rospy.loginfo('Waiting for the action server to start')
     client.wait_for_server()
 
+    rospy.loginfo('Action server started, sending the goal')
     goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = x
-    goal.target_pose.pose.position.y = y
-    goal.target_pose.pose.orientation.w = w
+
+    # set frame
+    goal.target_pose.header.frame_id = 'map'
+
+    # set position
+    goal.target_pose.pose.position.x = 0.0
+    goal.target_pose.pose.position.y = 0.0
+    # goal.target_pose.pose.position.z = 0.0
+
+    # set orientation
+    # goal.target_pose.pose.orientation.x = 0.0
+    # goal.target_pose.pose.orientation.y = 0.0
+    # goal.target_pose.pose.orientation.z = 1.0
+    goal.target_pose.pose.orientation.w = 1.0
+
     client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
+
+    rospy.loginfo('Waiting for the result')
+    client.wait_for_result()
+
+    if client.get_state() == GoalStatus.SUCCEEDED:
+        rospy.loginfo('Succeeded')
     else:
-        return client.get_result()
+        rospy.loginfo('Failed')
 
-
-def callback(data):
-    rospy.loginfo(data)
-    global pub
-    # pub.publish('Y')
-    
-    try:
-        result = movebase_client(-0.15, 2.0)
-        if result:
-            rospy.loginfo("Goal execution done!")            
-            pub.publish('Y')
-    except rospy.ROSInterruptException:
-        rospy.loginfo("Navigation test finished.")
-    
-def listener():
-    rospy.Subscriber("Intel", String, callback)
-    rospy.spin()
-
-if __name__ == '__main__': 
-    rospy.init_node('amm_talker', anonymous=True)
-    
-    time.sleep(2)
-    start_nav(0, 0, 0)
-    time.sleep(2)
-    
-
-    start_nav(1.0, 0, 0)
-    listener()
-    
+if __name__ == '__main__':
+    main()
